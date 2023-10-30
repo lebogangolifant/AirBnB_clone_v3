31beea69fdbfa30e4bf33a8d16d54aa2a1d5f701
Lebogang <lebogangodeveloper@gmail.com>
2023-10-30 15:18:32 +0200

    Fix code to be PEP8 compliant

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,25 +2,26 @@
 """RESTful API Application"""
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint # jsonify, make_response
+from flask import Flask, Blueprint  # jsonify, make_response
 from os import getenv
-#from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-#CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+# CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown(self):
     """Closes the database when the app context is torn down."""
     storage.close()
 
-"""
-@app.errorhandler(404)
-def not_found(error):
-    Custom 404 error response.
-    return jsonify({'error': 'Not found'}), 404
-"""
+
+# @app.errorhandler(404)
+# def not_found(error):
+#    Custom 404 error response.
+#    return jsonify({'error': 'Not found'}), 404
+
 
 if __name__ == '__main__':
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
