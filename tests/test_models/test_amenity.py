60e31e26a589d3e6d2c61cbce77d5dfaa9614cc2
Lebogang <lebogangodeveloper@gmail.com>
2023-10-31 08:39:03 +0000

    Update unittest

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -78,8 +78,7 @@ class TestAmenity(unittest.TestCase):
 
     def test_to_dict_creates_dict(self):
         """test to_dict method creates a dictionary with proper attrs"""
-        am = Amenity()
-	print(am.__dict__)
+        am = Amenity
         new_d = am.to_dict()
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
