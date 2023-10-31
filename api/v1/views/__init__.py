fe449a38491bdb47876f9c28ac549ada95a4c8fa
Lebogang <lebogangodeveloper@gmail.com>
2023-10-31 11:20:38 +0200

    Create a Blueprint with the /api/v1 prefix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 """create a variable app_views which is an instance of Blueprint"""
 from flask import Blueprint
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,5 +12,3 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
